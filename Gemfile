fc91eeb866994820696ab07865f62418a5d6cdd6
Stefano Verna <stefano.verna@welaika.com>
2011-09-27 22:48:07 +0200

    First spree installation

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'rails'
+gem 'mysql2', '~> 0.2.11'
+gem 'spree', '0.60.1'
+gem 'compass'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Antifasoul::Application.config.secret_token = '0c11e63985ce5b721ae1f26ecfaf6c3f195536b75495e3a9f7baad0ec4d4879ab7564172fd80e0647164ae6f946f1373ae0ddb0be1629a1cca26fff029122a95'
