a8c6ab9d6f4191b58eb633c7021edf0a7fb3b231
Stefano Verna <stefano.verna@welaika.com>
2011-10-21 02:53:56 +0200

    Tema

--- app/models/image_decorator.rb
+++
@@ -1,8 +0,0 @@
-Image.class_eval do
-  attachment_definitions[:attachment][:styles] = {
-    :mini         =>  '100x100#',
-    :small_thumb  =>  '216x216>',
-    :large_thumb  =>  '320x320>',
-    :large        =>  '600x600>'
-  }
-end
--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,6 +1,33 @@
 if Spree::Config.instance
   Spree::Config.set(:default_locale => 'it')
   Spree::Config.set(:allow_ssl_in_production => false)
+  Spree::Config.set(:logo => '/images/custom/logo.png')
 end
+
+def euro_price_filter
+  conds = [ [ "Sotto i €10",  "price             <= 10" ],
+            [ "€10 - €15",    "price between 10 and 15" ],
+            [ "€15 - €18",    "price between 15 and 18" ],
+            [ "€18 - €20",    "price between 18 and 20" ],
+            [ "€20 o più",    "price             >= 20" ] ]
+  { :name   => "Filtra per Prezzo",
+    :scope  => :price_range_any,
+    :conds  => Hash[*conds.flatten],
+    :labels => conds.map {|k,v| [k,k]}
+  }
+end
+
+::Taxon.class_eval do
+  def applicable_filters
+    [euro_price_filter]
+  end
+end
+
+Image.attachment_definitions[:attachment][:styles] = {
+  :mini         =>  '100x100#',
+  :small        =>  '216x216#',
+  :product      =>  '320x320>',
+  :large        =>  '600x600>'
+}
 
 I18n.available_locales = [:it, :en]
