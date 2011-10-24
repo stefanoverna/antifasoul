4d5f4e7b36a46a7aa446b946e61ad6b02292aa50
Stefano Verna <stefano.verna@welaika.com>
2011-10-24 22:00:21 +0200

    Migliorie alla grafica

---
+++ app/models/product_decorator.rb
@@ -0,0 +1,30 @@
+Product.class_eval do
+
+  include ActionView::Helpers::NumberHelper
+  include Spree::BaseHelper
+  include ProductsHelper
+
+  def option_values
+    option_types.map{|i| i.option_values }.flatten.uniq
+  end
+
+  def grouped_option_values
+    option_values.group_by(&:option_type)
+  end
+
+  def variant_options_hash
+    return @variant_options_hash if @variant_options_hash
+    @variant_options_hash = Hash[grouped_option_values.map{ |type, values|
+      [type.id.inspect, Hash[values.map{ |value|
+        [value.id.inspect, Hash[variants.includes(:option_values).select{ |variant|
+          variant.option_values.select{ |val|
+            val.id == value.id && val.option_type_id == type.id
+          }.length == 1 }.map{ |v| [ v.id, { :id => v.id, :count => v.count_on_hand, :price => product_price(v) } ] }]
+        ]
+      }]]
+    }]
+    @variant_options_hash
+  end
+
+end
+
