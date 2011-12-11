bac9fcc245dc309c8c62bfbfad792e8b2f4a01a9
Stefano Verna <stefano.verna@welaika.com>
2011-12-11 22:29:14 +0100

    Fix

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -1,11 +1,22 @@
 Order.class_eval do
 
-  alias_method :original_finalize!, :finalize!
-
   def finalize!
-    puts "ECCOCI"
+    logger.info 'finalize!'
+
+    update_attribute(:completed_at, Time.now)
+    self.out_of_stock_items = InventoryUnit.assign_opening_inventory(self)
+    # lock any optional adjustments (coupon promotions, etc.)
+    adjustments.optional.each { |adjustment| adjustment.update_attribute("locked", true) }
+
+    OrderMailer.confirm_email(self).deliver
     OrderMailer.confirm_email_to_admins(self).deliver
-    original_finalize!
+
+    self.state_events.create({
+      :previous_state => "cart",
+      :next_state     => "complete",
+      :name           => "order" ,
+      :user_id        => (User.respond_to?(:current) && User.current.try(:id)) || self.user_id
+    })
   end
 
 end
